31517d5de4efe8712056d21a33421a32786fa1fd
Anabel Montero Díaz <anabelmd@outlook.es>
2023-11-11 18:00:53 +0100

    cambios en js

--- src/animations.js
+++ src/animations.js
@@ -1,8 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const sections = document.querySelectorAll('section');
-  const datosPersonalesSection = document.getElementById('datos-personales');
-  let datosPersonalesVisible = false;
   let currentSectionIndex = 0;
+  let touchStartY;
 
   function showCurrentSection() {
     sections.forEach((section, index) => {
@@ -11,13 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  document.addEventListener('wheel', function(event) {
+  document.addEventListener('wheel', function (event) {
     const delta = Math.sign(event.deltaY);
 
     if (delta === 1 && currentSectionIndex < sections.length - 1) {
       currentSectionIndex++;
     } else if (delta === -1 && currentSectionIndex > 0) {
       currentSectionIndex--;
+    }
+
+    showCurrentSection();
+  });
+
+  document.addEventListener('touchstart', function (event) {
+    touchStartY = event.touches[0].clientY;
+  });
+
+  document.addEventListener('touchend', function (event) {
+    const touchEndY = event.changedTouches[0].clientY;
+    const delta = touchEndY - touchStartY;
+
+    if (delta > 50 && currentSectionIndex > 0) {
+      currentSectionIndex--;
+    } else if (delta < -50 && currentSectionIndex < sections.length - 1) {
+      currentSectionIndex++;
     }
 
     showCurrentSection();
