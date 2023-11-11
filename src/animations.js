9a5db61f78232e5ea89aea7855938a5668d8d804
Anabel Montero Díaz <anabelmd@outlook.es>
2023-11-11 18:39:42 +0100

    cambios en javascript

--- src/animations.js
+++ src/animations.js
@@ -10,39 +10,48 @@ document.addEventListener('DOMContentLoaded', function () {
     startY = event.touches[0].clientY;
   }
 
-  function handleTouchMove(event) {
-    const deltaY = event.touches[0].clientY - startY;
-
+  function handleTouchEnd() {
+    // Detecta la dirección del desplazamiento y actualiza el índice de la sección actual
+    const deltaY = event.changedTouches[0].clientY - startY;
     if (deltaY > 50 && currentSectionIndex > 0) {
       currentSectionIndex--;
     } else if (deltaY < -50 && currentSectionIndex < sections.length - 1) {
       currentSectionIndex++;
     }
 
-    showCurrentSection();
-  }
+    function handleTouchMove(event) {
+      const deltaY = event.touches[0].clientY - startY;
 
-  function showCurrentSection() {
-    sections.forEach((section, index) => {
-      const opacity = index === currentSectionIndex ? 1 : 0;
-      section.style.opacity = opacity;
-    });
-  }
+      if (deltaY > 50 && currentSectionIndex > 0) {
+        currentSectionIndex--;
+      } else if (deltaY < -50 && currentSectionIndex < sections.length - 1) {
+        currentSectionIndex++;
+      }
 
-  document.addEventListener('wheel', function (event) {
-    const delta = Math.sign(event.deltaY);
-
-    if (delta === 1 && currentSectionIndex < sections.length - 1) {
-      currentSectionIndex++;
-    } else if (delta === -1 && currentSectionIndex > 0) {
-      currentSectionIndex--;
+      showCurrentSection();
     }
+
+    function showCurrentSection() {
+      sections.forEach((section, index) => {
+        const opacity = index === currentSectionIndex ? 1 : 0;
+        section.style.opacity = opacity;
+      });
+    }
+
+    document.addEventListener('wheel', function (event) {
+      const delta = Math.sign(event.deltaY);
+
+      if (delta === 1 && currentSectionIndex < sections.length - 1) {
+        currentSectionIndex++;
+      } else if (delta === -1 && currentSectionIndex > 0) {
+        currentSectionIndex--;
+      }
+
+      showCurrentSection();
+    });
+    document.addEventListener('touchstart', handleTouchStart);
+    document.addEventListener('touchstart', handleTouchStart);
+    document.addEventListener('touchmove', handleTouchMove);
 
     showCurrentSection();
   });
-
-  document.addEventListener('touchstart', handleTouchStart);
-  document.addEventListener('touchmove', handleTouchMove);
-
-  showCurrentSection();
-});
